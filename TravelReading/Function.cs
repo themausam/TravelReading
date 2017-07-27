7c26d0c6d6a2f75763c3db7807ded3e0b1ec8ea6
Mausam Shrestha <mshrestha@st-marys-ascot.co.uk>
2017-07-27 16:10:42 +0100

    First commit.

--- TravelReading/Function.cs
+++ TravelReading/Function.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Alexa.NET.Request;
+using Alexa.NET.Response;
 using Amazon.Lambda.Core;
+using Alexa.NET.Request.Type;
+using System.Net.Http;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -12,16 +15,50 @@ namespace TravelReading
 {
     public class Function
     {
-        
-        /// <summary>
-        /// A simple function that takes a string and does a ToUpper
-        /// </summary>
-        /// <param name="input"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        private static HttpClient _httpClient;
+
+        public const string INVOCATION_NAME = "Reading Travel";
+
+        public Function()
         {
-            return input?.ToUpper();
+            _httpClient = new HttpClient();
+        }
+
+        public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
+        {
+            var requestType = input.GetRequestType();
+            if (requestType == typeof(IntentRequest))
+            {
+                var intentRequest = input.Request as IntentRequest;
+                var postcodeRequested = intentRequest.Intent.Slots["postcode"].Value;
+                return MakeSkillResponse($"You want to know about { postcodeRequested }. I will look that up for you.", true);
+            }
+            else
+            {
+                return MakeSkillResponse($"I don't know how to handle this intent. Please say something like Alexa, ask { INVOCATION_NAME } for nearby bus stops", true);
+            }
+        }
+
+        private SkillResponse MakeSkillResponse(string outputSpeech, bool shouldEndSession, string repromptText = "Reprompting. To exit, say, exit.")
+        {
+            var response = new ResponseBody
+            {
+                ShouldEndSession = shouldEndSession,
+                OutputSpeech = new PlainTextOutputSpeech { Text = outputSpeech }
+            };
+
+            if (repromptText != null)
+            {
+                response.Reprompt = new Reprompt() { OutputSpeech = new PlainTextOutputSpeech() { Text = repromptText } };
+            }
+
+            var skillReponse = new SkillResponse
+            {
+                Response = response,
+                Version = "1.0"
+            };
+
+            return skillReponse;
         }
     }
 }
